be398a41a83f03e610cc387aecd4d70b0868eff1
ns <notanemail@notanemail.notanemail>
2019-09-09 23:15:16 +0800

    changeBackground finish

--- changeBackground/changeBackground.bash
+++ changeBackground/changeBackground.bash
@@ -1,4 +1,9 @@
 #! /bin/bash
+if [ -f ~/.bashrc ];
+then
+  . ~/.bashrc
+fi
+source /etc/profile
 picPath=/home/zgk/Pictures/	# assign picPath your own picture directory as a value
 cd $picPath
 picNum=`ls -l | grep "^-" | wc -l`
@@ -13,7 +18,8 @@ done
 
 result=`expr $ramNum % $picNum`
 echo background:${folderList[$result]} >> /home/zgk/start1.log		# if you want to see which one it makes, you can remove "#"
-gsettings set org.gnome.desktop.background picture-uri $picPath${folderList[$result]}
+#gsettings set org.gnome.desktop.background picture-uri $picPath${folderList[$result]}
+sudo -H -u zgk DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus gsettings set org.gnome.desktop.background picture-uri $picPath${folderList[$result]}
 
 sleep 0.1
 a=0
@@ -24,4 +30,5 @@ done
 
 result=$(expr $[ $result + $a ] % $picNum)
 echo screensaver:${folderList[$result]}	>> /home/zgk/start2.log # if you want to see which one it makes, you can remove "#"
-gsettings set org.gnome.desktop.screensaver picture-uri $picPath${folderList[$result]}
+#gsettings set org.gnome.desktop.screensaver picture-uri $picPath${folderList[$result]}
+sudo -H -u zgk DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus gsettings set org.gnome.desktop.screensaver picture-uri $picPath${folderList[$result]}
