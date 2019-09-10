5bc032ab9bb715aac9b467290389bf3c0e9d5f27
ns <notanemail@notanemail.notanemail>
2019-09-10 22:03:13 +0800

    maybe finish

--- changeBackground/changeBackground.bash
+++ changeBackground/changeBackground.bash
@@ -1,13 +1,11 @@
 #! /bin/bash
-if [ -f ~/.bashrc ];
-then
-  . ~/.bashrc
-fi
 source /etc/profile
+env LANG=en_US.UTF-8
 picPath=/home/zgk/Pictures/	# assign picPath your own picture directory as a value
 cd $picPath
 picNum=`ls -l | grep "^-" | wc -l`
 ramNum=`date +%s`		# develop a random integer
+ramNum=`expr $ramNum + $RANDOM `
 j=0
 folderList[0]=a
 for i in `ls -1`		# fill an array with the names of pictures
@@ -17,18 +15,11 @@ do
 done
 
 result=`expr $ramNum % $picNum`
-echo background:${folderList[$result]} >> /home/zgk/start1.log		# if you want to see which one it makes, you can remove "#"
+echo background:${folderList[$result]} `date`>> /home/zgk/program/Bash/changeBackground/start1.log		# if you want to see which one it makes, you can remove "#"
 #gsettings set org.gnome.desktop.background picture-uri $picPath${folderList[$result]}
 sudo -H -u zgk DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus gsettings set org.gnome.desktop.background picture-uri $picPath${folderList[$result]}
 
-sleep 0.1
-a=0
-while  [ "$a" -eq "0" ]
-do
-	a=`date +%s`
-done
-
-result=$(expr $[ $result + $a ] % $picNum)
-echo screensaver:${folderList[$result]}	>> /home/zgk/start2.log # if you want to see which one it makes, you can remove "#"
+result=$(expr $[ $result * $RANDOM ] % $picNum)
+echo screensaver:${folderList[$result]} `date`	>> /home/zgk/program/Bash/changeBackground/start2.log # if you want to see which one it makes, you can remove "#"
 #gsettings set org.gnome.desktop.screensaver picture-uri $picPath${folderList[$result]}
 sudo -H -u zgk DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus gsettings set org.gnome.desktop.screensaver picture-uri $picPath${folderList[$result]}
