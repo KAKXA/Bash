cf5abb7189c8c4c3c3b0976acf237f8440793780
ns <notanemail@notanemail.notanemail>
2019-09-12 09:41:54 +0800

    finish maybeeee

--- changeBackground/changeBackground.bash
+++ changeBackground/changeBackground.bash
@@ -1,6 +1,5 @@
 #! /bin/bash
 source /etc/profile
-env LANG=en_US.UTF-8
 picPath=/home/zgk/Pictures/	# assign picPath your own picture directory as a value
 cd $picPath
 picNum=`ls -l | grep "^-" | wc -l`
@@ -15,11 +14,11 @@ do
 done
 
 result=`expr $ramNum % $picNum`
-echo background:${folderList[$result]} `date`>> /home/zgk/program/Bash/changeBackground/start1.log		# if you want to see which one it makes, you can remove "#"
+echo background:${folderList[$result]} `env LANG=en_US.UTF-8 date`>> /home/zgk/program/Bash/changeBackground/start1.log		# if you want to see which one it makes, you can remove "#"
 #gsettings set org.gnome.desktop.background picture-uri $picPath${folderList[$result]}
 sudo -H -u zgk DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus gsettings set org.gnome.desktop.background picture-uri $picPath${folderList[$result]}
 
 result=$(expr $[ $result * $RANDOM ] % $picNum)
-echo screensaver:${folderList[$result]} `date`	>> /home/zgk/program/Bash/changeBackground/start2.log # if you want to see which one it makes, you can remove "#"
+echo screensaver:${folderList[$result]} `env LANG=en_US.UTF-8 date`	>> /home/zgk/program/Bash/changeBackground/start2.log # if you want to see which one it makes, you can remove "#"
 #gsettings set org.gnome.desktop.screensaver picture-uri $picPath${folderList[$result]}
 sudo -H -u zgk DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus gsettings set org.gnome.desktop.screensaver picture-uri $picPath${folderList[$result]}
