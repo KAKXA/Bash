e6786301e6df9c77efc4d85bbac39f369dc99498
ns <notanemail@notanemail.notanemail>
2020-09-03 11:35:11 +0800

    caps lock

---
+++ disp/to222.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output HDMI-1 --auto --primary
+xrandr --output eDP-1 --off
--- disp/turn.sh
+++ disp/turn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 disp=`xrandr | grep 2560x1440+0+0`
 if [ -n "$disp" ];then
-	xrandr --output HDMI-1-1 --rotate left
+	xrandr --output HDMI-1 --rotate left
 else
-	xrandr --output HDMI-1-1 --rotate normal
+	xrandr --output HDMI-1 --rotate normal
 fi
